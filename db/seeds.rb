3938fdce30e062ad4fce40f23c4003b31fe8e5f5
trinityXmontoya <trinityXmontoya@gmail.com>
2014-10-03 06:10:25 -0400

    add seeds for heroku deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,38 @@
-# Category.create!(id: 1,name: "Breakfast")
-# Category.create!(id: 2,name: "Lunch")
-# Category.create!(id:3,name: "Dinner")
-#
-# Testimonial.create!(name: "Dan",
-#                     event: "Dinner Party",
-#                     comment: "Loved it, most delicious meal I've ever had and my guests loved it! Hannah's dad rocks.")
-# Testimonial.create!(name: "Twitter",
-#                     event: "Office party",
-#                     comment: "Nothing but good things to say, professional *and* fun, exquisite meal.")
-#
-# Testimonial.create!(name: "Hannah",
-#                     event: "Birthday party",
-#                     comment: "He's my dad but, he's really the best there is! Recommend him 100%")
+Category.create!(id: 1,name: "Breakfast")
+Category.create!(id: 2,name: "Lunch")
+Category.create!(id:3,name: "Dinner")
 
-# MenuItem.create!(title: "Challah French Toast",
-#                  description: "with homemade agave maple syrup",
-#                  category_id: 1)
-# MenuItem.create!(title: "Hamburger",
-#                  description: "with toasted sesame bun",
-#                  category_id: 2)
-# MenuItem.create!(title: "Lobster",
-#                  description: "with claws",
-#                  category_id: 3)
-# MenuItem.create!(title: "omelette du fromage",
-#                 description: "over easy",
-#                 category_id: 1)
-# MenuItem.create!(title: "Sweet Potatoe Fries",
-#                 description: "with aioli mayo",
-#                 category_id: 2)
-# MenuItem.create!(title: "Clam chowder",
-#                 description: "as the Bostonians do it",
-#                 category_id: 3)
+Testimonial.create!(name: "Dan",
+                    event: "Dinner Party",
+                    comment: "Loved it, most delicious meal I've ever had and my guests loved it! Will definitely call again."
+                    approved: true)
+Testimonial.create!(name: "Twitter",
+                    event: "Office party",
+                    comment: "Nothing but good things to say, professional *and* fun, exquisite meal."
+                    approved: true)
+
+Testimonial.create!(name: "Hannah",
+                    event: "Birthday party",
+                    comment: "He's my dad but, he's really the best there is! Recommend him 100%")
+
+MenuItem.create!(title: "Challah French Toast",
+                 description: "with homemade agave maple syrup",
+                 category_id: 1)
+MenuItem.create!(title: "Hamburger",
+                 description: "with toasted sesame bun",
+                 category_id: 2)
+MenuItem.create!(title: "Lobster",
+                 description: "with claws",
+                 category_id: 3)
+MenuItem.create!(title: "omelette du fromage",
+                description: "over easy",
+                category_id: 1)
+MenuItem.create!(title: "Sweet Potatoe Fries",
+                description: "with aioli mayo",
+                category_id: 2)
+MenuItem.create!(title: "Clam chowder",
+                description: "as the Bostonians do it",
+                category_id: 3)
 SiteInfo.create(
   id: 1,
   header_img: '/images/header-bg.jpg',
@@ -47,10 +49,3 @@ SiteInfo.create(
   contact_byline: 'Lorem ipsum dolor sit amet consectetur.',
   menu_bg_img: '/images/food.jpg'
 )
-#
-# GalleryImage.create(url:'/images/food1.jpg')
-# GalleryImage.create(url:'/images/food2.jpg')
-# GalleryImage.create(url:'/images/food3.jpg')
-# GalleryImage.create(url:'/images/food4.jpg')
-# GalleryImage.create(url:'/images/food5.jpg')
-# GalleryImage.create(url:'/images/food6.jpg')
